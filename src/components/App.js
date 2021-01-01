20ccc7e14da6b5d90d65fa95288123224757d26a
Jeroen Zwart <jeroen.zwart@live.com>
2021-01-01 16:00:39 +0100

    added redux connect to the songlist

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SongList from './SongList';
 
 const App = () => {
   return (
     <div>
-      App
-    </div>)
+      <SongList />
+    </div>
+    )
 };
 
 export default App;
