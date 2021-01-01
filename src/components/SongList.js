e9dcc1a4744b74d877a41d19b7962c0ef3803b47
Jeroen Zwart <jeroen.zwart@live.com>
2021-01-01 20:13:42 +0100

    created list items

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,10 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.item}>
+          <div className="right floated content">
+            <div className="ui button primary">Select</div>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+        );
+    });
+  };
+
   render() {
-    return <div>SongList</div>;
+    return <div className="ui devided list">{this.renderList()}</div>
   }
-}
+};
+
+
 
 const mapStateToProps = (state) => {
   return { songs: state.songs };
